0959a17fdaaf647e427ee908758ee0fab2c91126
Tran Duc Quy <tranducquylhp@gmail.com>
2019-10-23 16:02:56 +0700

    ok

--- server.js
+++ server.js
@@ -12,25 +12,25 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 var server = http.createServer(app);
 
 
 app.get('/', (req, res) => {
-    res.send("Home page. Server running okay.");
+  res.send("Home page. Server running okay.");
 });
 
 app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === 'ma_xac_minh_cua_ban') {
-        res.send(req.query['hub.challenge']);
-    }
-    res.send('Error, wrong validation token');
+  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
 });
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
 server.listen(app.get('port'), app.get('ip'), function() {
-    console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
 });
